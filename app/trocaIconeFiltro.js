960fdf5c96b9977a9f5762a1c16cb406e180abcc
ThiagoZambelli <thiagozamoliveira@gmail.com>
2023-02-15 17:27:37 -0300

    Criação da troca de btn de filtro alem do ajuste da postagem e escolha de cores

--- app/trocaIconeFiltro.js
+++ app/trocaIconeFiltro.js
@@ -1,12 +1,27 @@
 const btnFiltroContainer = document.querySelector("#btn-filtro-container");
 
-btnFiltroContainer.addEventListener('click', (e)=>{  
+
+
+
+
+btnFiltroContainer.addEventListener('click', (e)=>{
+    console.log(e.target.attributes.src.value)
+ 
+    if(btnAberto(e)){
+        e.target.attributes.src.value = "img/btn-filtro.png";
+    }else if(btnFechado(e)){
+        e.target.attributes.src.value = "img/btn-filtro-aberto.png";
+    }
     
-    if(e.originalTarget.attributes.src.value === "img/btn-filtro-aberto.png"){
-    e.originalTarget.attributes.src.value = "img/btn-filtro.png";
-        }else if(e.originalTarget.attributes.src.value === "img/btn-filtro.png"){
-        e.originalTarget.attributes.src.value = "img/btn-filtro-aberto.png";
-        }
 })
 
 
+
+
+function btnFechado(e) {
+    return e.target.attributes.src.value === "img/btn-filtro.png";
+}
+
+function btnAberto(e) {
+    return e.target.attributes.src.value === "img/btn-filtro-aberto.png";
+}
