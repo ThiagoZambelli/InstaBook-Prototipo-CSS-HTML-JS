b54567a8876a3a9c4d2fb8ba4bb49a589470ce16
ThiagoZambelli <thiagozamoliveira@gmail.com>
2023-02-16 16:51:06 -0300

    Busca dos autores direto na API (JsonPlaceholder) e link entre autor e postagem utilizando um forEatch para postar uma a uma das postagens e uma função que busca o autor pelo userId na postagem direto na API

--- app/conectaApi.js
+++ app/conectaApi.js
@@ -1,13 +1,30 @@
 const requisicaoPosts = 'https://jsonplaceholder.typicode.com/posts';
+const requisicaoUsuarios = 'https://jsonplaceholder.typicode.com/users';
 
 
 async function buscaPostagens(){
  const res = await fetch(requisicaoPosts);
  const postagens = res.json();
 
- return postagens
+ return postagens;
+}
+
+async function buscaUsuarios(){
+    const res = await fetch(requisicaoUsuarios);
+    const usuarios = res.json();
+
+    return usuarios;
+}
+
+async function buscarUsuarioUnico(id){
+    const res = await fetch(requisicaoUsuarios+`?id=${id}`)
+    const usuario = res.json();   
+
+    return usuario;
 }
 
 export const conexao = {
-    buscaPostagens
+    buscaPostagens,
+    buscaUsuarios,
+    buscarUsuarioUnico
 }
--- app/mostrarPostagem.js
+++ app/mostrarPostagem.js
@@ -4,12 +4,13 @@ async function mostrarTela(lista){
     const areaDePostagem = document.querySelector("#feed");
     const listaPostagens = await lista;
     areaDePostagem.innerHTML = "";
-    listaPostagens.forEach(e => {
+    listaPostagens.forEach(async e => {
+        const autor = await buscarUsuario(e.userId)        
         areaDePostagem.innerHTML+=`
         <section class="postagem flex__container">
                 <div class="postagem-cabecalho flex__container">
                     <img src="img/avatar-ausente.png" alt="Avatar do usuario" class="postagem-avatar" id="postagem-avatar" data-postagem="avatar">
-                    <h3 class="postagem-autor" id="postagem-autor" data-postagem="autor">Teste Autor</h3>
+                    <h3 class="postagem-autor" id="postagem-autor" data-postagem="autor">${autor}</h3>
                 </div>
                 <div class="postagem-corpo flex__container">
                     <h2 class="postagem-titulo" id="postagem-titulo" data-postagem="titulo">${e.title}</h2>
@@ -22,8 +23,13 @@ async function mostrarTela(lista){
         `
     })
 
-    console.log(listaPostagens)
+    
 
+}
+async function buscarUsuario(id){
+    const autorDaPostagem = await conexao.buscarUsuarioUnico(id);
+    const autorDaPostagemTraduzido = autorDaPostagem[0].name;
+    return autorDaPostagemTraduzido;
 }
 
 mostrarTela(conexao.buscaPostagens())
